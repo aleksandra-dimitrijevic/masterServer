2ac98fce61bd2cbc9dbbdc02b39c85b4abb73066
UNDPCO\aleksandra.dimitrije <aleksandra.dimitrijevic@undp.org>
2022-08-15 13:56:47 +0200

    Encode password

--- routes/users.js
+++ routes/users.js
@@ -8,12 +8,14 @@ const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const auth = require("../middleware/auth");
 
-const multer  = require('multer');
-const upload = multer({ dest: 'uploads/',
+const multer = require('multer');
+const upload = multer({
+  dest: 'uploads/',
   filename: function (req, file, cb) {
-  const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-  cb(null, file.fieldname + '-' + uniqueSuffix );
-}});
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  }
+});
 
 mongoose.Promise = global.Promise;
 
@@ -51,73 +53,81 @@ router.post('/', async (req, res) => {
     if (user) {
       return res.status(404).send({ msg: "Already exists" });
     }
-    //Encrypt user password
-    //encryptedPassword = await bcrypt.hash(req.body.password, 10);
+    
+    encryptedPassword = await bcrypt.hash(req.body.password, 10);
 
     user = new User({
       firstName: req.body.firstName,
-      password: req.body.password,
+      password: encryptedPassword,
       lastName: req.body.lastName,
       email: req.body.email.toLowerCase(),
       phone: req.body.phone,
       role: 'user',
-      ratesNumber:0,
-      score:0,
-      rates:[]
+      ratesNumber: 0,
+      score: 0,
+      rates: []
     });
 
-    // save model to database
-    user.save(function (err, p) {
-      if (err) return console.error(err);
-      console.log("Saved to  collection.");
-    });
+    await user.save()
+
     res.send({ user });
   } catch (err) {
-    // If an error occurred, send it to the client
     res.json(err);
   }
 });
 router.post('/login', async (req, res) => {
-  //if (user && (await bcrypt.compare(password, user.password)))
   try {
-    const user = await User.findOne({ email: req.body.email.toLowerCase(), password: req.body.password })
-    if (!user) return res.status(404).send({ msg: "Bad username or password" });
-    // Create token
-    const token = jwt.sign(
-      { user_id: user._id, email: user.email.toLowerCase() },
-      JWTsecret,
-      {
-        expiresIn: "12h",
-      }
-    );
-    // save user token
-    user.token = token;
-    await user.save();
-    res.send({ user });
+    const user = await User.findOne({ email: req.body.email.toLowerCase() })
+
+    if (user && (await bcrypt.compare(req.body.password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email: user.email.toLowerCase() },
+        JWTsecret,
+        {
+          expiresIn: "12h",
+        }
+      );
+
+      user.token = token;
+      await user.save();
+      res.send({ user });
+    } else return res.status(404).send({ msg: "Bad username or password" });
+
   }
   catch (err) {
-    // If an error occurred, send it to the client
     console.log(err)
     res.json(err);
   }
 });
 
-router.patch('/:id', auth, async(req, res) => {
+router.patch('/:id', auth, async (req, res) => {
   try {
-    var user = await User.findOneAndUpdate({_id: req.params.id}, {...req.body}, { new: true})
+    var user = await User.findOneAndUpdate({ _id: req.params.id }, { ...req.body }, { new: true })
     res.send({ user });
   } catch (err) {
     res.json(err);
   }
 })
 
-router.patch('/rate/:id', auth, async(req, res) => {
+router.patch('/password/:id', auth, async (req, res) => {
   try {
-    const {userRated, rate} = req.body
-    var user = await User.findOne({_id: req.params.id})
-    if(!user) res.send({msg:'User not found!'})
-    const rateIndex = user.rates.findIndex( r => r.user == userRated)
-    if(rateIndex != -1){
+    encryptedPassword = await bcrypt.hash(req.body.password, 10);
+    var user = await User.findOneAndUpdate({ _id: req.params.id }, { password: encryptedPassword}, { new: true })
+    res.send({ user });
+  } catch (err) {
+    console.log(err)
+    res.json(err);
+  }
+})
+
+router.patch('/rate/:id', auth, async (req, res) => {
+  try {
+    const { userRated, rate } = req.body
+    var user = await User.findOne({ _id: req.params.id })
+    if (!user) res.send({ msg: 'User not found!' })
+    const rateIndex = user.rates.findIndex(r => r.user == userRated)
+    if (rateIndex != -1) {
       user.score += rate - user.rates[rateIndex].rate;
       user.rates[rateIndex].rate = rate;
     } else {
@@ -133,13 +143,14 @@ router.patch('/rate/:id', auth, async(req, res) => {
 
     res.send({ user });
   } catch (err) {
+    console.log(err);
     res.json(err);
   }
 })
 
 router.post('/picture', upload.single('avatar'), async (req, res, next) => {
-  await User.findOneAndUpdate({_id: req.body.user}, {image: req.file.filename})
-  res.send({file: req.file.filename})
+  await User.findOneAndUpdate({ _id: req.body.user }, { image: req.file.filename })
+  res.send({ file: req.file.filename })
 })
 
 router.get('/picture/:name', async (req, res) => {
