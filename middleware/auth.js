9209bfc40366be60556c7abad0894cf22523cb70
UNDPCO\aleksandra.dimitrije <aleksandra.dimitrijevic@undp.org>
2022-08-15 11:17:59 +0200

    Change Messages for verifying token

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,14 +4,14 @@ const JWTsecret = process.env.JWT_SECRET || 'secret';
 const verifyToken = (req, res, next) => {
   const token = req.body.token || req.query.token || req.headers["x-access-token"];
   if (!token) {
-    return res.status(403).send({msg: "A token is required for authentication"});
+    return res.status(403).send({msg: "A token is required for authentication, please log in."});
   }
   try {
     const decoded = jwt.verify(token, JWTsecret);
     req.user = decoded;
   } catch (err) {
     console.log(err)
-    return res.status(401).send({msg: "Invalid Token"});
+    return res.status(401).send({msg: "Invalid Token, please log in."});
   }
   return next();
 };
